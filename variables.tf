2725ca49a8425d9138f42a8d5d35fe28cefa30ee
LolaNeh <loisekmokua@gmail.com>
2023-09-20 20:05:56 -0500

    terraform

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "my-region" {
+    type =string
+    default = "us-east-1"
+
+}
+
+variable "profile" {
+    default = "default"
+
+}
+
+variable "topic_name" {
+    default = "sns-dev"
+  
+}
+
+variable "Team" {
+    default = "dev"
+  
+}
+
+variable "protocol_type" {
+    default = "email"
+  
+}
+
+variable "endpoint" {
+    default = "ghghghghghgh@gmail.com"
+  
+}
---
+++ version.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = var.my-region
+  profile = var.profile
+}
